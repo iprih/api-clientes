13dd5e828f54ed77aa44aae04f3246c6fc3fe65a
Priscila Silva <43504804+iprih@users.noreply.github.com>
2019-11-22 14:08:45 -0300

    atualizando rotas

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const controller = require("../controller/clientesController")
 
 
-router.post("/", controller.post)
+
 router.get("/", controller.get)
 router.get("/compradores", controller.getCompradores)
+router.post("/", controller.post)
 router.get("/:cpf", controller.getCpf)
+router.put("/:cpf", controller.updateCliente)
 
 module.exports = router
